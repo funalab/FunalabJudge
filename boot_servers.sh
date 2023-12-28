53b356f161b874bb8d531e04322fc0e0b67bb81f
Ryota Kimura <rkimura@fun.bio.keio.ac.jp>
2023-12-28 20:44:54 +0900

    fix chakra ui env

--- boot_servers.sh
+++ boot_servers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/rm -rf frontend/node_modules && /bin/mkdir frontend/node_modules && docker-compose up -d --build frontend
+/bin/rm -rf frontend/node_modules && /bin/mkdir frontend/node_modules && docker-compose up frontend
