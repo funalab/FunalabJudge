efcab50e9c64770635aa105a83366606c081dc0f
Taichi Araki <araki@fun.bio.keio.ac.jp>
2024-02-05 14:19:15 +0900

    Dockerfile_backend modified to use cache.

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-  "github.com/gin-contrib/cors"
 )
 
 // レスポンスとして返すデータ
@@ -14,22 +16,21 @@ func main() {
 	// Ginルーターを作成
 	router := gin.Default()
 
-  config := cors.DefaultConfig()
+	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173"} // リクエストを許可するオリジンを指定
 	router.Use(cors.New(config))
-	
-  // エンドポイントのハンドラー関数を設定
+
+	// エンドポイントのハンドラー関数を設定
 	router.GET("/", func(c *gin.Context) {
 		// レスポンスデータの作成
 		data := Data{
 			Message: "Hello from Gin!!",
 		}
-
 		// JSON形式でレスポンスを返す
 		c.JSON(200, data)
 	})
 
 	// サーバーをポート3000で起動
 	router.Run(":3000")
+	fmt.Println("Server is running.")
 }
-
