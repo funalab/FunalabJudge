e0d9cd8f984c537797ae57f9a52d0b86911c9af4
Ryota Kimura <rkimura@fun.bio.keio.ac.jp>
2024-02-08 14:47:01 +0900

    fix chakura theme import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
-
 // ルーティング設定に必要なものをimport
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,18 +10,16 @@ import { Schedule } from "./routes/Schedule";
 
 export const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />}/> 
-          <Route path="/account" element={<Account />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/message" element={<Message />} />
-          <Route path="/schedule" element={<Schedule />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />}/> 
+        <Route path="/account" element={<Account />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/schedule" element={<Schedule />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
