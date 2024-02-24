0af0e3e1fd87f2c6f7734aa1f446f46460fb8546
Taichi Araki <araki@fun.bio.keio.ac.jp>
2024-02-24 22:19:38 +0900

    add multiple file functionality

--- frontend/src/components/SubmitForm.tsx
+++ frontend/src/components/SubmitForm.tsx
@@ -1,7 +1,6 @@
-import { Text, Stack, Divider, Flex, Button, HStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Text, Stack, Divider, Flex, HStack, Input } from '@chakra-ui/react'
+import { useState } from 'react'
 import SubmitButton from './SubmitButton'
-import SubmitFile from './SubmitFile'
 
 export interface SubmitFormProps {
   problemId: number
@@ -10,42 +9,25 @@ export interface SubmitFormProps {
 const SubmitForm: React.FC<SubmitFormProps> = ({ problemId }) => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([])
 
-  const handleSelectedFiles = (file: File) => {
-    setSelectedFiles(prevFiles => [...prevFiles, file])
+  const handleInputFile = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const files: File[] = Array.from(ev.target.files!)
+    handleSelectedFiles(files)
   }
 
-  const [inputFields, setInputFields] = useState<JSX.Element[]>([
-    <SubmitFile handleSelectedFiles={handleSelectedFiles} />
-  ]);
-
-  const handlePlus = () => {
-    const newInputFields = [...inputFields];
-    newInputFields.push(
-      <SubmitFile handleSelectedFiles={handleSelectedFiles} />
-    );
-    setInputFields(newInputFields);
-  };
-
-  const handleMinus = () => {
-    if (inputFields.length === 1) {
-      return;
-    }
-    const newInputFields = [...inputFields];
-    newInputFields.pop();
-    setInputFields(newInputFields);
-  };
-
+  const handleSelectedFiles = (files: File[]) => {
+    files.map((file) => {
+      setSelectedFiles(prevFiles => [...prevFiles, file])
+    })
+  }
   return (
     <>
       <Divider />
       <HStack>
         <Text fontSize={30} fontWeight={'bold'}>Submit Form</Text>
-        <Button onClick={handlePlus}>+</Button>
-        <Button onClick={handleMinus}>-</Button>
       </HStack>
       <Stack>
         <Stack>
-          {inputFields}
+          <Input type="file" onChange={handleInputFile} multiple />
         </Stack>
         <Flex>
           <SubmitButton selectedFiles={selectedFiles} problemId={problemId} />
